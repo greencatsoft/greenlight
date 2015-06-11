7ee004e7dcfbc904a1c869b90bca38474e06ed92
mmx900 <mmx900@gmail.com>
2015-06-11 21:12:12 +0900

    Add missing parameter to Exception

--- shared/main/scala/com/greencatsoft/greenlight/package.scala
+++ shared/main/scala/com/greencatsoft/greenlight/package.scala
@@ -5,5 +5,5 @@ import com.greencatsoft.greenlight.grammar.Statement
 package object greenlight {
 
   case class TestFailureException(
-    message: String, statement: Option[Statement] = None) extends Exception
+    message: String, statement: Option[Statement] = None) extends Exception(message)
 }
